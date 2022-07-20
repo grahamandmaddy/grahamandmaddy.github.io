b037aae2c9b5fed22a7a78eb5169f1700559cff8
grahamjantz <grahammrjantz@gmail.com>
2022-07-20 16:13:56 -0600

    story and details added and js drop down menu implemented

---
+++ main.js
@@ -0,0 +1,27 @@
+let menuButton = document.getElementById('menuButton');
+let navBar = document.getElementById('navBarFull');
+let mainContainer = document.getElementById('mainContainer');
+let mainDiv = document.getElementById('mainDiv');
+let navCloseButton = document.getElementById('navCloseButton');
+
+menuButton.addEventListener('click', () => {
+    showMenu();
+})
+
+navCloseButton.addEventListener('click', () => {
+    hideMenu();
+})
+
+function showMenu () {
+    navBar.style.display = 'grid';
+    navBar.style.backgroundColor = 'var(--main-rust)';
+    menuButton.style.display= 'none';
+    navBar.style.animation = 'slideIn 1s';
+    navBar.style.top = '0px';
+}
+
+function hideMenu() {
+    navBar.style.animation = 'slideOut 1s';
+    menuButton.style.display = 'block';
+    navBar.style.top = '-500px';
+}
