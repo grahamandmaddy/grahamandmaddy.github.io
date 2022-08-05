b3b8915dc26324c5cd3e649d23cfd34eced70588
grahamjantz <grahammrjantz@gmail.com>
2022-08-05 11:27:57 -0600

    fix nav issues, add animation to drop down menu button on mobile

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-let menuButton = document.getElementById('menuButton');
 let navBar = document.getElementById('navBarFull');
 let mainContainer = document.getElementById('mainContainer');
 let mainDiv = document.getElementById('mainDiv');
-let navCloseButton = document.getElementById('navCloseButton');
+const navMenu = document.querySelector("#navMenu");
 
-menuButton.addEventListener('click', () => {
-    showMenu();
-})
-
-navCloseButton.addEventListener('click', () => {
-    hideMenu();
-})
 
 function showMenu () {
-    //navBar.style.display = 'grid';
     navBar.style.backgroundColor = 'var(--main-rust)';
-    menuButton.style.display= 'none';
     navBar.style.animation = 'slideIn 1s';
     navBar.style.top = '0px';
 }
 
 function hideMenu() {
     navBar.style.animation = 'slideOut 1s';
-    menuButton.style.display = 'block';
     navBar.style.top = '-500px';
 }
+
+navMenu.addEventListener("click", () => {
+    if (navMenu.className == "inactive") {
+        navMenu.className= "active";
+        showMenu();
+    } else {
+        navMenu.className = "inactive";
+        hideMenu();
+    }
+})
